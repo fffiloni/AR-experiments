5e670ed294b1ea3aacd2bb71d869501a276f5a0a
fffiloni <sylvain.fillion15@gmail.com>
2018-07-01 09:37:45 +0200

    first commit

---
+++ ARjs-gabarits/examples/js-essentials/essentials-ar-scripts.js
@@ -0,0 +1,3 @@
+<script src="https://aframe.io/releases/0.8.0/aframe.min.js"></script>
+<script src="https://jeromeetienne.github.io/AR.js/aframe/build/aframe-ar.js"></script>
+<script>THREEx.ArToolkitContext.baseURL = "https://rawgit.com/jeromeetienne/ar.js/master/three.js/"</script>
---
+++ ARjs-gabarits/examples/js-essentials/essentials-three_scripts.js
@@ -0,0 +1,8 @@
+//three.js library
+<script src="https://cdn.rawgit.com/jeromeetienne/AR.js/0694b414/three.js/examples/vendor/three.js/build/three.min.js"></script>
+<script src="https://cdn.rawgit.com/jeromeetienne/AR.js/0694b414/three.js/examples/vendor/three.js/examples/js/libs/stats.min.js"></script>
+//ar.js
+<script src="https://cdn.rawgit.com/jeromeetienne/AR.js/0694b414/three.js/build/ar.js"></script>
+<script>
+  THREEx.ArToolkitContext.baseURL = "https://github.com/jeromeetienne/AR.js/tree/master/three.js/";
+</script>
